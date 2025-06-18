3e8bd08a4013a4062edf11120e1831a3a5a9202b
unknown <jabezayugu04@gmail.com>
2025-06-18 11:24:24 +0300

    Prettier clean up

--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -1,81 +1,83 @@
-import {Request, Response} from 'express';
-import {PrismaClient, tasks} from '@prisma/client';
+import { Request, Response } from "express";
+import { PrismaClient, tasks } from "@prisma/client";
 
 const client = new PrismaClient();
 
-
 export const getAllTasks = async (_req: Request, res: Response) => {
-      try {
+  try {
     const tasks = await client.tasks.findMany();
     res.status(200).json(tasks);
-  }catch (_e) {
-    res.status(500).json({ error: 'Something went wrong! Please try again later!' })
+  } catch (_e) {
+    res
+      .status(500)
+      .json({ error: "Something went wrong! Please try again later!" });
   }
 };
 
 export const createTask = async (req: Request, res: Response) => {
-    try{
-        const { title, description}: tasks = req.body;
-        const newTask = await client.tasks.create({
-            data: {
-                title,
-                description,
-                isCompleted: false
-            },
-        });
-        res.status(201).json(newTask);
-    }catch (e) {
-        res.status(500).json({ error: 'Something went wrong! Please try again later!' });
-    }
+  try {
+    const { title, description }: tasks = req.body;
+    const newTask = await client.tasks.create({
+      data: {
+        title,
+        description,
+        isCompleted: false,
+      },
+    });
+    res.status(201).json(newTask);
+  } catch (e) {
+    res
+      .status(500)
+      .json({ error: "Something went wrong! Please try again later!" });
+  }
 };
 
 export const getTaskById = async (req: Request, res: Response) => {
-     try {
+  try {
     const { id } = req.params;
     const task = await client.tasks.findUnique({
       where: { id: String(id) },
     });
     if (task) {
-        res.status(200).json(task);
-    }else{
-       res.status(404).json({ message: 'Task was not found' });
+      res.status(200).json(task);
+    } else {
+      res.status(404).json({ message: "Task was not found" });
     }
   } catch (e) {
-    res.status(500).json({ message: 'Something went wrong. Try again Later!' });
+    res.status(500).json({ message: "Something went wrong. Try again Later!" });
   }
 };
 
-
 export const updateTask = async (req: Request, res: Response) => {
-    try {
+  try {
     const { id } = req.params;
     const { title, description, isCompleted } = req.body;
     const task = await client.tasks.update({
       where: {
-         id: String(id)
-         },
-      data: { 
+        id: String(id),
+      },
+      data: {
         title: title && title,
         description: description && description,
         isCompleted: isCompleted && isCompleted,
-         },
+      },
     });
     res.status(200).json(task);
   } catch (e) {
-    console.error('Error updating task:', e);
-    res.status(500).json({ message: 'Something went wrong. Try again Later!' });
+    console.error("Error updating task:", e);
+    res.status(500).json({ message: "Something went wrong. Try again Later!" });
   }
 };
 
 export const deleteTask = async (req: Request, res: Response) => {
-     const { id } = req.params;
+  const { id } = req.params;
   try {
     await client.tasks.delete({
       where: { id: String(id) },
     });
     res.status(204).send();
   } catch (e) {
-    console.error('Error deleting task:', e);
-    res.status(500).json({ message: 'Something went wrong. Try again Later!' });
+    console.error("Error deleting task:", e);
+    res.status(500).json({ message: "Something went wrong. Try again Later!" });
   }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
-import express from 'express';
-import { getAllTasks, createTask, getTaskById, updateTask, deleteTask} from './controllers/tasks.controller'; 
+import express from "express";
+import {
+  getAllTasks,
+  createTask,
+  getTaskById,
+  updateTask,
+  deleteTask,
+} from "./controllers/tasks.controller";
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (_req, res) => {
-  res.send('<h1>Welcome to Task API!</h1>');
+app.get("/", (_req, res) => {
+  res.send("<h1>Welcome to Task API!</h1>");
 });
 
-app.get('/tasks', getAllTasks);
-app.post('/tasks', createTask);
-app.get('/tasks/:id', getTaskById);
-app.patch('/tasks/:id', updateTask);
-app.delete('/tasks/:id', deleteTask);
+app.get("/tasks", getAllTasks);
+app.post("/tasks", createTask);
+app.get("/tasks/:id", getTaskById);
+app.patch("/tasks/:id", updateTask);
+app.delete("/tasks/:id", deleteTask);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
